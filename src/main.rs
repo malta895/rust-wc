084c395d52a9e92a2aa1388e153aeab37d733f8e
malta895 <lmalta95@gmail.com>
2024-06-30 23:07:48 +0200

    fix; step one completed

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,22 @@ fn main() {
         eprintln!("Usage: rust-wc -c <file>");
         std::process::exit(1);
     }
-    let file_name = &args[1].as_str();
+    let file_name = &args[2].as_str();
     match count_bytes(file_name){
         Ok(count) => println!("{} {}", count, file_name),
-        Err(e) => eprintln!("Error: {}", e),
+        Err(e) =>{
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        },
     }
-
 }
 
 
 fn count_bytes(file_name: &str) -> io::Result<usize> {
+    println!("file_name: {}", file_name);
     match std::fs::File::open(file_name) {
         Ok(file) => Ok(file.bytes().count()),
         Err(e) => Err(e),
     }
-
 }
 
