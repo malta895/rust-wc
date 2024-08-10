4410842d9c0533c41951a67e12ba89ae93b899de
malta895 <lmalta95@gmail.com>
2024-08-10 11:31:17 +0200

    mimic real wc behavior and order

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,34 @@ fn main() {
     match result {
         Ok(args) => {
             let file_name = args.file_name;
-            if args.bits & ArgParser::BYTES != 0 {
-                let bytes = count_bytes(&file_name).unwrap();
-                println!("{} {}", bytes, file_name);
-            }
+            let mut has_printed: bool = false;
+
             if args.bits & ArgParser::LINES != 0 {
                 let lines = count_lines(&file_name).unwrap();
-                println!("{} {}", lines, file_name);
+                print_val(&mut has_printed, lines);
             }
+
+            if args.bits & ArgParser::BYTES != 0 {
+                let bytes = count_bytes(&file_name).unwrap();
+                print_val(&mut has_printed, bytes);
+            }
+
+            print!(" {}\n", file_name);
         }
         Err(e) => {
             eprintln!("{}", e);
             print_usage(true);
         }
-    } 
-   
+    }    
+}
 
-    
+
+fn print_val(has_printed: &mut bool, val: usize) {
+    if *has_printed {
+        print!(" ");
+    }
+    print!("{}", val);
+    *has_printed = true;
 }
 
 fn print_usage(with_error: bool) {
