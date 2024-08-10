bfd9d5cccfb1d9cd2b1781cd8aee44f2723bfcb5
malta895 <lmalta95@gmail.com>
2024-08-10 11:20:05 +0200

    count lines and bytes

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,54 @@
-use std::{env, io::{self, Read}};
+use std::{env, io::{self, BufRead, Read},};
+
+use rust_wc::ArgParser;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: rust-wc -c <file>");
-        std::process::exit(1);
+    let args = &args[1..];
+    if args.len() < 2 {
+        print_usage(true);
     }
-    let file_name = &args[2].as_str();
-    match count_bytes(file_name){
-        Ok(count) => println!("{} {}", count, file_name),
-        Err(e) =>{
-            eprintln!("Error: {}", e);
-            std::process::exit(1);
-        },
+    let result = ArgParser::from_args(args.to_vec());
+
+    match result {
+        Ok(args) => {
+            let file_name = args.file_name;
+            if args.bits & ArgParser::BYTES != 0 {
+                let bytes = count_bytes(&file_name).unwrap();
+                println!("{} {}", bytes, file_name);
+            }
+            if args.bits & ArgParser::LINES != 0 {
+                let lines = count_lines(&file_name).unwrap();
+                println!("{} {}", lines, file_name);
+            }
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            print_usage(true);
+        }
+    } 
+   
+
+    
+}
+
+fn print_usage(with_error: bool) {
+    eprintln!("Usage: rust-wc [-c -l -w -m] <file>");
+    if with_error {
+        std::process::exit(1);
     }
 }
 
 
+fn count_lines(file_name: &str) -> io::Result<usize> {
+    let file = std::fs::File::open(file_name)?;
+    let buf_reader = io::BufReader::new(file);
+    Ok(buf_reader.lines().count())
+}
+
 fn count_bytes(file_name: &str) -> io::Result<usize> {
-    println!("file_name: {}", file_name);
-    match std::fs::File::open(file_name) {
-        Ok(file) => Ok(file.bytes().count()),
-        Err(e) => Err(e),
-    }
+    let file = std::fs::File::open(file_name)?;
+    let buf_reader = io::BufReader::new(file);
+    Ok(buf_reader.bytes().count())
 }
 
