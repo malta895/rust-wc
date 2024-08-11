58bc34badc175438883f59c704144a287cb9322a
malta895 <lmalta95@gmail.com>
2024-08-11 23:40:01 +0200

    improve efficiency

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod arg_parser;
 
+use arg_parser::ArgParser;
 use std::{
     env,
     fs::File,
-    io::{self, BufRead, BufReader, Read, Seek},
+    io::{self, BufRead, BufReader, Read, Seek}, ops::Bound,
 };
-use arg_parser::ArgParser;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -23,7 +23,7 @@ fn main() {
 
             match std::fs::File::open(file_name.clone()) {
                 Ok(file) => {
-                    buf_reader = io::BufReader::new(file);
+                    buf_reader = io::BufReader::new( file)
                 }
                 Err(e) => {
                     eprintln!("{}", e.to_string());
@@ -32,14 +32,20 @@ fn main() {
                 }
             }
 
+            if args.bits & ArgParser::LINES != 0 && args.bits & ArgParser::BYTES != 0 {
+                let (lines, bytes) = count_lines_and_bytes(&mut buf_reader);
+                println!("{} {} {}", lines, bytes, file_name);
+                std::process::exit(0);
+            }
+
             if args.bits & ArgParser::LINES != 0 {
                 let len = count_lines(&mut buf_reader);
                 print_val(&mut has_printed, len);
                 has_printed = true;
-                if let Err(err) = buf_reader.seek(io::SeekFrom::Start(0)){
+                if let Err(err) = buf_reader.rewind() {
                     eprintln!("Error while reading file {}: {}", file_name, err);
                     print_usage(true);
-                    return
+                    return;
                 }
             }
 
@@ -78,5 +84,21 @@ fn count_lines(buf_reader: &mut BufReader<File>) -> usize {
 }
 
 fn count_bytes(buf_reader: &mut BufReader<File>) -> usize {
-    buf_reader.bytes().count()
+    let mut buf: Vec<u8> = Vec::new();
+    let read = buf_reader.read_to_end(&mut buf);
+    read.unwrap()
+}
+
+fn count_lines_and_bytes(buf_reader: &mut BufReader<File>) -> (usize, usize) {
+    let mut lines_total: usize = 0;
+    let mut bytes_total: usize = 0;
+    let mut str_buf = String::new();
+    while let Ok(bytes_count) = buf_reader.read_line(&mut str_buf) {
+        if bytes_count == 0 {
+            break;
+        }
+        lines_total += 1;
+        bytes_total += bytes_count;
+    }
+    (lines_total, bytes_total)
 }
